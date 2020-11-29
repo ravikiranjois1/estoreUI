ac0c2bad166e73d23ab7f67b2b9aea19b7be98c6
Ravikiran Jois <51014349+ravikiranjois1@users.noreply.github.com>
2020-11-29 16:19:17 -0500

    updated constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const rest_endpoint = 'https://le16raojhl.execute-api.us-east-1.amazonaws.com/dev/';
-export const accessKeyId = 'AKIAYUPXS4UXVNYE2PD2';
-export const secretAccessKey = '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';
+export const rest_endpoint = 'https://le16raojhl.execute-api.us-east-1.amazonaws.com/dev';
+export const accessKeyId = 'ASIASDF5LFVUISJGZD7D';
+export const secretAccessKey = 'zMFrLfHSJyoe7MnH3n7ZfXyzdO1V2wMnLTubmYAR';
